93753abed9aac10a83906427afa62c1502793b89
Andrew Mackenzie <38409574+amackenzie1@users.noreply.github.com>
2018-05-01 20:16:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int input[] = {1, 2, 3, 5, 5};
-        int input2 [] = {0, 0, 0, 6, 6};
+        int input[] = {1, 2, 3, 4, 5};
+        int input2 [] = {2, 2, 2, 6, 6};
 
 
         System.out.println(occurrences(input, 5));
         System.out.println(max_occurrences(input2)[1]);
+        System.out.println(size(input));
 
 
-        int rankings1 [] = {5, 5, 5, 5, 11};
-        int rankings2 [] = {5, 5, 5, 5, 8};
+        int rankings1 [] = {2, 2, 2, 5, 7};
+        int rankings2 [] = {2, 2, 2, 7, 6};
+        
         int suits1 [] = {1, 2, 3, 3, 4};
         int suits2 [] = {4, 3, 3, 3, 3};
 
@@ -17,8 +19,6 @@ public class Main {
         System.out.println(hand_comparison(rankings1, rankings2, suits1, suits2));
 
     }
-
-    //counts occurences of y in arrray x
 
     private static int occurrences(int[] x, int y){
         int r = 0;
@@ -30,7 +30,7 @@ public class Main {
         return r;
     }
 
-    //outputs the number that occurs the most as an array: {number, occurrences}, unless the number is 0 in which case it is ignored.
+    //outputs the number that occurs the most as {number, occurrences}, unless the number is 0.
 
     private static int[] max_occurrences(int[] x){
         int[] r = {0, 0};
@@ -45,17 +45,20 @@ public class Main {
     //removes all occurrences of y and replaces them with 0
     //I'm doing this because I think removing items from an array and not replacing them with anything is impossible in Java, right?
 
-    private static int[] remove(int[] x, int y){
-        int[] dummyx = new int[x.length]
-        for (int number = 0; number < 5; number++){
-            dummyx[number] = x[number]
-            if (x[number] == y){
+
+
+    private static int[] remove(int[] x, int y) {
+        int[] dummyx = new int[x.length];
+        for (int number = 0; number < 5; number++) {
+            dummyx[number] = x[number];
+            if (x[number] == y) {
                 dummyx[number] = 0;
             }
         }
         return dummyx;
     }
-    private static int maximum(int[] x){
+
+        private static int maximum(int[] x){
         int r = 0;
         for(int number = 0; number < 5; number++){
             if (x[number] > r){
@@ -74,8 +77,8 @@ public class Main {
         }
         return r;
     }
-    
-   private static int size(int[] x){
+
+    private static int size(int[] x){
         int r = 0;
         r = r + (maximum(x)*50625);
         x = remove(x, maximum(x));
@@ -90,15 +93,25 @@ public class Main {
         return r;
 
 
+
+
+
+
+
+
+
+
+    }
+
     //finally the main function
     //if player 1 has the better cards, returns 1. if player 2 has the better cards, it returns 2, and if they are equal, returns 0.
     //suits are 1: spades 2: hearts 3: clubs 4: diamonds
-    //rankings are 2 through 14 
-    
+    //rankings are 2 through 14
+
     private static int hand_comparison(int[] rankings1, int[] rankings2, int[] suits1, int[] suits2){
-        
-        // insert straight-flush checker here, because straight-flush is better than 4-of-a-kind, just a bit of a pain to program 
-       
+        // insert royal-flush checker here
+
+
 
 
         //checking four-of-a-kind
@@ -136,68 +149,68 @@ public class Main {
                 }
             }
         }
-        
-        
-        
+
+
+
         // check for the rest of possibilities, using a similar process to above
         //in order from best to worst:
-        
+
         //full house
         boolean h1HasFH = false;
         boolean h2HasFH = false;
-        
-        if(max_occurences(rankings1)[1] == 3){
-            dummy_rankings1 = remove(rankings1,max_occurences(rankings1)[0]);
-            if(max_occurences(dummy_rankings1)[1] == 2){
+
+        if(max_occurrences(rankings1)[1] == 3){
+            dummy_rankings1 = remove(rankings1,max_occurrences(rankings1)[0]);
+            if(max_occurrences(dummy_rankings1)[1] == 2){
                 h1HasFH = true;
-                
+
             }
-              
+
         }
-        
-         if(max_occurences(rankings2)[1] == 3){
-            dummy_rankings2 = remove(rankings2,max_occurences(rankings2)[0]);
-            if(max_occurences(dummy_rankings2)[1] == 2){
+
+        if(max_occurrences(rankings2)[1] == 3){
+            dummy_rankings2 = remove(rankings2,max_occurrences(rankings2)[0]);
+            if(max_occurrences(dummy_rankings2)[1] == 2){
                 h2HasFH = true;
-                
+
             }
-              
+
         }
-        
+
         if (h1HasFH && !h2HasFH){
             return 1;
         }
-        
+
         if(h2HasFH && !h1HasFH){
             return 2;
         }
-        
+
         if(h1HasFH && h2HasFH){
-            if (max_occurences(rankings1)[0] > max_occurences(ranking2)[0]){
+            if (max_occurrences(rankings1)[0] > max_occurrences(rankings2)[0]){
                 return 1;
             }
-            
-            if(max_occurences(rankings2)[0] > max_occurences(rankings1)[0]){
+
+            if(max_occurrences(rankings2)[0] > max_occurrences(rankings1)[0]){
                 return 2;
             }
-            
-            if(max_occurences(dummy_rankings1)[0] > max_occurences(dummy_rankings2)[0]){
+
+            if(max_occurrences(dummy_rankings1)[0] > max_occurrences(dummy_rankings2)[0]){
                 return 1;
             }
-            
-            if(max_occurences(dummy_rankings2)[0] > max_occurences(dummy_rankings1)[0]){
+
+            if(max_occurrences(dummy_rankings2)[0] > max_occurrences(dummy_rankings1)[0]){
                 return 2;
             }
-            
+
             return 0;
-            
-                
-            
+
+
+
         }
-        
-        
-                
-        //flush
+
+
+        // check for the rest of possibilities, using a similar process to above
+
         if (max_occurrences(suits1)[1] == 5 && max_occurrences(suits2)[1] != 5){
             return 1;
         }
@@ -217,10 +230,9 @@ public class Main {
                 return 0;
             }
         }
-                
- 
-        //straight
-        
+
+        // straight
+
         if ((max_occurrences(rankings1)[1] == 1 && maximum(rankings1) - minimum(rankings1) == 4) && !(max_occurrences(rankings2)[1] == 1 && maximum(rankings2) - minimum(rankings2) == 4)){
             return 1;
         }
@@ -239,10 +251,10 @@ public class Main {
                 return 0;
             }
         }
-        
-        //three-of-a-kind
-        
-        
+
+        // three of a kind
+
+
         if (max_occurrences(rankings1)[1] == 3 && max_occurrences(rankings2)[1] != 3){
             return 1;
         }
@@ -289,13 +301,5 @@ public class Main {
                 }
             }
         }
-        //two pairs
-        //one pair
-        //high card (meaning nothing)
-        
-        // this place is still completely incomplete
-        
         return 0; //if we weren't able to determine a winner the hands are equal
     }
-}
-
