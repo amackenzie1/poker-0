91ebcd6661f7d3850ed82f62578247d70f116a80
Sophia Roshal <SophiaSR@users.noreply.github.com>
2018-07-24 20:06:28 -0400

    Added card class

---
+++ Card.java
@@ -0,0 +1,18 @@
+public class Card {
+	int ranking;
+	int suite;
+	public Card (int ranking, int suite) {
+		this.ranking = ranking;
+		this.suite = suite;
+	}
+	
+	public int getRanking() {
+		return ranking;
+	}
+	
+	public int getSuite() {
+		return suite;
+	}
+	
+
+}
